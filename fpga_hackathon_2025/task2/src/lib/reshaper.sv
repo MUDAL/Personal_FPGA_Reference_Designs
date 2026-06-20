8fd28eeb16c811daf219f2c719d8c0ee3b6b9af1
MUDAL <eolaoluwaraji@gmail.com>
2026-06-20 20:59:43 +0100

    [RJX]: Made the reshaper module more compact.

--- fpga_hackathon_2025/task2/src/lib/reshaper.sv
+++ fpga_hackathon_2025/task2/src/lib/reshaper.sv
@@ -33,53 +33,43 @@ module reshaper
    output    logic [ADDR_LEN-1:0] o_mem_addr,
    output    logic                o_valid);
    
-   logic   [ADDR_LEN-1:0] max_reg;
-   logic   [ADDR_LEN-1:0] max_next;
-   logic   [ADDR_LEN-1:0] row_reg;
-   logic   [ADDR_LEN-1:0] row_next;
-   logic   [ADDR_LEN-1:0] col_reg1;
-   logic   [ADDR_LEN-1:0] col_next1;
-   logic   [ADDR_LEN-1:0] col_reg2;
-   logic   [ADDR_LEN-1:0] col_next2;   
-   logic [2*ADDR_LEN-1:0] prod_reg;
-   logic [2*ADDR_LEN-1:0] prod_next;
-   logic [2*ADDR_LEN:0  ] sum_reg;
-   logic [2*ADDR_LEN:0  ] sum_next;
-   logic            [1:0] i_vreg;   
-   logic                  o_vreg;   
+   typedef struct
+   {
+      logic   [ADDR_LEN-1:0] max;
+      logic   [ADDR_LEN-1:0] row;
+      logic   [ADDR_LEN-1:0] col1;
+      logic   [ADDR_LEN-1:0] col2;
+      logic [2*ADDR_LEN-1:0] prod;
+      logic [2*ADDR_LEN:0  ] sum;
+   }data_t; // Datapath registers
+   data_t data_reg;
+   data_t data_next;
+   
+   logic [1:0] i_vreg;   
+   logic       o_vreg;   
    
    // Loading input registers
-   assign max_next   = (i_valid)   ?  i_row_max : max_reg;
-   assign row_next   = (i_valid)   ?  i_row     : row_reg;
-   assign col_next1  = (i_valid)   ?  i_col     : col_reg1;
+   assign data_next.max   = (i_valid)   ?  i_row_max : data_reg.max;
+   assign data_next.row   = (i_valid)   ?  i_row     : data_reg.row;
+   assign data_next.col1  = (i_valid)   ?  i_col     : data_reg.col1;
    // Pipeline - 1st stage 
-   assign col_next2  =  col_reg1;
-   assign prod_next  = (i_vreg[0]) ?  max_reg * row_reg  : prod_reg;
+   assign data_next.col2  =  data_reg.col1;
+   assign data_next.prod  = (i_vreg[0]) ?  data_reg.max * data_reg.row  : data_reg.prod;
    // Pipeline - 2nd stage
-   assign sum_next   = (i_vreg[1]) ? prod_reg + col_reg2 : sum_reg;
+   assign data_next.sum   = (i_vreg[1]) ? data_reg.prod + data_reg.col2 : data_reg.sum;
    
    // Top-level outputs
-   assign o_mem_addr = sum_reg[ADDR_LEN-1:0];
-   assign o_valid    =  o_vreg;
+   assign o_mem_addr = data_reg.sum[ADDR_LEN-1:0];
+   assign o_valid    = o_vreg;
    
    always_ff @(posedge i_rst,posedge i_clk) begin: registers
       if(i_rst) begin
-         max_reg   <= {     ADDR_LEN{1'b0}};
-         row_reg   <= {     ADDR_LEN{1'b0}};
-         col_reg1  <= {     ADDR_LEN{1'b0}};
-         col_reg2  <= {     ADDR_LEN{1'b0}};         
-         prod_reg  <= {   2*ADDR_LEN{1'b0}};
-         sum_reg   <= { 2*ADDR_LEN+1{1'b0}};
-         i_vreg    <=          2'b0;
-         o_vreg    <=          1'b0;
+         data_reg  <=  '{default:0};
+         i_vreg    <=      2'b0;
+         o_vreg    <=      1'b0;
       end
       else begin
-         max_reg   <=   max_next;
-         row_reg   <=   row_next;
-         col_reg1  <=   col_next1;
-         col_reg2  <=   col_next2;       
-         prod_reg  <=   prod_next;
-         sum_reg   <=   sum_next;
+         data_reg  <=   data_next;
          i_vreg[0] <=   i_valid;
          i_vreg[1] <=   i_vreg[0];
          o_vreg    <=   i_vreg[1];
