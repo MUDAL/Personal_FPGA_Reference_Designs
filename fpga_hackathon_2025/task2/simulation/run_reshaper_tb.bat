da25eb46eb4a55d4ead2921c54fd3a14de21ae29
MUDAL <eolaoluwaraji@gmail.com>
2026-09-22 10:51:49 +0100

    [RJX]: Deleted reshaper batch file. It's now called flatten.

--- fpga_hackathon_2025/task2/simulation/run_reshaper_tb.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-mkdir build
-cd build
-echo:
-echo =============================== Running Reshaper Testbench ===============================
-echo:
-vsim -c -do "do ../scripts/reshaper_tb.do"
