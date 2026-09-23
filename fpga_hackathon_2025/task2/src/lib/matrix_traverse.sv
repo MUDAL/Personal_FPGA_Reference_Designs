a6ddeb659ea8f6b53e3151f17dc8eb6b991c38cf
MUDAL <eolaoluwaraji@gmail.com>
2026-09-23 17:42:27 +0100

    [RJX]: Removed unnecessary logic from the 'DIAGONAL' state.

--- fpga_hackathon_2025/task2/src/lib/matrix_traverse.sv
+++ fpga_hackathon_2025/task2/src/lib/matrix_traverse.sv
@@ -102,24 +102,18 @@ module matrix_traverse
             end
          end
          
-         DIAGONAL: begin
-            if(last_element) begin                                 
-               state_next =     IDLE;
-               i_reg      = '{default:0};  
-            end 
-            else begin              
-               if(o_reg.diag_down) begin
-                  i_reg.row = o_reg.row + 1'b1;
-                  i_reg.col = o_reg.col - 1'b1;
-                  if(o_reg.row == i_row_max - 2)                   state_next = HORIZONTAL;
-                  else if(o_reg.col == {{ADDR_LEN-1{1'b0}}, 1'b1}) state_next = VERTICAL;                 
-               end
-               else begin
-                  i_reg.row = o_reg.row - 1'b1;
-                  i_reg.col = o_reg.col + 1'b1;
-                  if(o_reg.col == i_col_max - 2)                   state_next = VERTICAL; 
-                  else if(o_reg.row == {{ADDR_LEN-1{1'b0}}, 1'b1}) state_next = HORIZONTAL;                             
-               end
+         DIAGONAL: begin             
+            if(o_reg.diag_down) begin
+               i_reg.row = o_reg.row + 1'b1;
+               i_reg.col = o_reg.col - 1'b1;
+               if(o_reg.row == i_row_max - 2)                   state_next = HORIZONTAL;
+               else if(o_reg.col == {{ADDR_LEN-1{1'b0}}, 1'b1}) state_next = VERTICAL;                 
+            end
+            else begin
+               i_reg.row = o_reg.row - 1'b1;
+               i_reg.col = o_reg.col + 1'b1;
+               if(o_reg.col == i_col_max - 2)                   state_next = VERTICAL; 
+               else if(o_reg.row == {{ADDR_LEN-1{1'b0}}, 1'b1}) state_next = HORIZONTAL;                             
             end
          end
          
